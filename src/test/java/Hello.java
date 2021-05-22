1dba05ad57ff7edb3be11ba3e2560e3ae166ff54
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-05-22 12:46:27 -0400

    committing all the work

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,6 +3,12 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World ");
         //enhancing hello
+<<<<<<< HEAD
         //work 1
+        //work 2
+        //work 3
+
+=======
+>>>>>>> parent of 765daf4 (work 1 is done)
     }
 }
---
+++ src/test/java/ResetPractice.java
@@ -0,0 +1,8 @@
+public class ResetPractice {
+
+
+    //work 4 is done
+    // work 5 is done after resetting hard way
+
+    //committing all together
+}
